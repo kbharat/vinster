7388add1f4d5ef76d3663abaf5320992c1a1312c
Bharat Khetan <kbharat@yahoo.com>
2014-03-30 15:30:12 +0000

    Before Images upload

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,22 @@ class PostsController < ApplicationController
 		#everything here will get run when someone goes to /posts
 		@posts = Post.all
 	end
+
+	def new
+	@post= Post.new
+	end
+
+	def create
+		Post.create( post_params )
+		# Post.create(:title => "omg", :description => "llalalla") what we used
+		redirect_to posts_path
+		#here we add the other line
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :description)
+	end
+
 end
